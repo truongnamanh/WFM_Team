c49ec01885341a4dd61161569ed89fbf7be6d27f
truongnamanh <132613050+truongnamanh@users.noreply.github.com>
2023-07-06 20:36:24 +0700

    Add files via upload

---
+++ 08.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 22:42:51 2023
+
+@author: v3180901
+"""
+
+a= 5
+b= 2
+c= a/b
+print(c)
+print("kiểu dữ liệu của a: ",type(a))
+print("kiểu dữ liệu của b: ",type(b))
+print("kiểu dữ liệu của c: ",type(c))
+
+
+n = 100
+m = "200"
+print(str(n)+m)
+print(n+int(m))
